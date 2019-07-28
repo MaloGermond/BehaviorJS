b07085017821ff80b60b609e30a3b8dc27d68d1e
Malo <Malo.germond@privowny.com>
2019-07-28 17:42:01 +0200

    State can be add to an object State can be animate and cycling Start document for convert css elements like borderWidth to border-width Lot of bug to fix ;)

--- library/frame.js
+++ library/frame.js
@@ -1,15 +1,22 @@
-class Component{
-  constructor(src){
-    this.states = new Object()
-    this.states["default"] = {
-      src:src
-    }
+var cssConvert = {
+  borderWidth : "border-width",
+  borderRadius : "border-radius",
+  borderColor: "border-color",
+  borderStyle: "border-style",
+  backgroundColor: "background-color",
+  boxShadow: "box-shadow"
+}
 
-    this.el = document.createElement('img')
-    this.el.setAttribute("src",src)
+class Component{
+  constructor(){
+    this.states = new Object()
+
+    this.el = document.createElement('div')
 
     this.el.style.width = "50px"
     this.el.style.height = "50px"
+    this.el.style.backgroundColor = "#61b1ff"
+    this.el.style.borderRadius = "8px"
 
     this.el.style.transition = "all 1s"
     this.el.style.position = "relative"
@@ -18,7 +25,9 @@ class Component{
     document.body.appendChild(this.el)
 
     this.el.addEventListener("click", this.onTap.bind(this))
-    // this.el.addEventListener("mouseover", this.onHover.bind(this))
+    this.el.addEventListener("mouseover", this.onOver.bind(this))
+    this.el.addEventListener("mousein", this.onOver.bind(this))
+    this.el.addEventListener("mouseout", this.onLeave.bind(this))
   }
 
   // addState(name, src){
@@ -40,62 +49,67 @@ class Component{
     this.tap()
   }
 
-  onHover(){
-    this.hover()
+  onOver(){
+    this.lastState = this.onState
+    this.over()
+  }
+
+  onLeave(){
+    // console.log(this.lastState);
+    this.animate(this.lastState)
   }
 
   animate(state){
-    // console.log(state);
+    console.log(state);
     // console.log(this.states[state])
 
-
     let obj = this.states[state]
+    this.onState = state
+
+    // for (let arg in this.el.style) {
+    //   console.log(arg);
+    // }
+
     for (let el in obj) {
       if(el == "src"){
-
+        this.el.setAttribute("src",this.states[state].src)
       }
-      this.el.style.setProperty(el, obj[el])
+
+      if(el == 'scale'){
+        // not supported
+        // this.el.style.width = this.el.style.width.slice(0, -2) * obj[el] + "px"
+        // this.el.style.height = this.el.style.height.slice(0, -2)*obj[el] + "px"
+      }
+
+      if(cssConvert[el] != null){
+        this.el.style.setProperty(cssConvert[el], obj[el])
+      }else{
+        this.el.style.setProperty(el, obj[el])
+      }
+
     }
     // for (var i = 0; i < Object.values(this.states[state]).length; i++) {
     //   console.log(this.states[state])
     // }
-    if(state in this.states){
-      this.el.setAttribute("src",this.states[state].src)
-    }else{
-      // console.log("state: "+ state+" dosen't exist");
-    }
+    // if(state in this.states && this.states[state].src != null){
+    //
+    // }else{
+    //   // console.log("state: "+ state+" dosen't exist");
+    // }
   }
 
   cycle() {
     // console.log(arguments);
-    // console.log(this.states);
+    // console.log(this.onState)
     // console.log(this.lastState);
-
-    for (let i = 0; i < arguments.length; i++){
-      if(this.lastState == null || i >= arguments.length-1){
-        this.animate(arguments[0])
-        this.lastState = 0
-        break
-      }
-
-      if (i == this.lastState) {
+    for (let i = 0; i < arguments.length-1; i++){
+      if (arguments[i] == this.onState) {
+        // console.log(arguments[i+1]);
+        // this.lastState = arguments[i]
         this.animate(arguments[i+1])
-        this.lastState = i+1
-        break
       }
     }
+    this.animate(arguments[0])
   }
 
-  updateStyle(){
-    let obj = this.style
-    for (let el in obj) {
-      this.el.style.setProperty(el, obj[el])
-    }
-    // console.log(this.el.style);
-  }
-}
-class Animation {
-  constructor() {
-
-  }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,48 @@
+var rect
+
 window.onload = function setup() {
-  let shapes = document.getElementsByClassName('shape')
-  for (el of shapes) {
-    el.addEventListener("mouseover", function(){testEvent("over")})
-    el.addEventListener("mouseleave", function(){testEvent("out")})
-    el.addEventListener("mousedown", function(){testEvent("down")})
-    el.addEventListener("mousemove", function(){testEvent("move")})
+  rect = new Component()
+
+  rect.states["default"]={
+    width: "100px",
+    height: "100px",
+    borderWidth: "4px",
+    borderRadius: "8px",
+    backgroundColor: "#61b1ff",
+    top: "50vh",
+    left: "50vw"
   }
+
+  rect.states["hover"]={
+    //boxShadow: "0px 0px 8px"
+    backgroundColor: "aqua",
+    borderRadius: "100%"
+  }
+
+  rect.animate("default")
+
+
+  rect.tap = function() {
+    // console.log("tap");
+    rect.animate({
+      top: Math.random(0,innerHeight)+"px",
+      width: Math.random(0,innerWidth)+"px"
+    })
+    //rect.cycle("hover","default")
+  }
+
+  rect.over = function(){
+    // console.log("hover")
+    rect.animate("hover")
+  }
+
 }
 
-function testEvent(type){
-  console.log(type);
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function random(){
+  return Math.random() * (max - min) + min;
 }
